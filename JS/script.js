f7f2fe7b526ba056ac34865f93910900e9de1b72
natyxflow <natyflores123@hotmail.com>
2021-04-19 22:04:24 -0300

    validação de formulário

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,50 @@
 
 
 
+function validation() {
+    if (document.getElementById('name').value.length<3) {
+        alert("Por favor, informe o seu nome!");
+        document.getElementById("name").focus();
+        return false;
+    }
+
+    if (document.getElementById("email").value.length<9){
+        alert("Por favor, informe um endereço de e-mail válido!");
+        document.getElementById("email").focus();
+        return false;
+    }
+
+    if (document.getElementById("cpf").value.length!=11){
+        alert("Por favor, informe um CPF válido!");
+        document.getElementById("cpf").focus();
+        return false;
+    }
+
+    else {
+        alert("Cadastro realizado com sucesso!")
+    }
+    return true;
+}
+
+function validations() {
+    if (document.getElementById('friendname').value.length<3) {
+        alert("Por favor, informe o nome do seu amigo!");
+        document.getElementById("friendname").focus();
+        return false;
+    }
+
+    if (document.getElementById("friendmail").value.length<9){
+        alert("Por favor, informe um endereço de e-mail válido!");
+        document.getElementById("friendmail").focus();
+        return false;
+    }
+
+    else {
+        alert("Convite realizado com sucesso!")
+    }
+    return true;
+}
+
 function callApi(url) {
     let request = new XMLHttpRequest()
     request.open("GET", url, false)
